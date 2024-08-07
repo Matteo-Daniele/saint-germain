82fcc6a37cd6083907d5262fd13aff1a1ecc0f92
Matteo Daniele <matteodaniele222@gmail.com>
2024-08-07 16:36:05 -0300

    I added new functionalities to collection store, when i touch in a type of product the collection store will show that type of product in the window

--- src/components/itemsToSell.js
+++ src/components/itemsToSell.js
@@ -1,6 +1,13 @@
-import classNames from "classnames"
-
+import { almuerzos, panes, patisserie, viennoiseries } from "@/lib/products";
+import classNames from "classnames";
 export default function ItemsToSell({verificating}){
+    const mainImageLink =[
+        'https://saintgermain.uy/cdn/shop/collections/Viennoiserie-categoria_900x.jpg',
+        "https://saintgermain.uy/cdn/shop/collections/panes-categoria_900x.jpg",
+        "https://saintgermain.uy/cdn/shop/collections/Painsuisse_1080x.jpg",
+        "https://saintgermain.uy/cdn/shop/collections/Quichequesodecabra_1080x.jpg",
+    ] 
+    const textTypeProduct = ['VIENNOISERIES', 'PANES', 'PATISSERIE', 'ALMUERZOS']; 
     const imageStyle = [
         "h-[150px] w-full object-cover"
     ]
@@ -19,30 +26,30 @@ export default function ItemsToSell({verificating}){
     return(
         <div className={firstContainerStyle}>
             <div className={fatherStyle}>
-                <a href="../collectionTienda" className={sonStyle}>
+                <a href={`../collectionTienda?mainImage=${mainImageLink[0]}&typeProduct=${textTypeProduct[0]}&products=${viennoiseries}`} className={sonStyle}>
                     <div className="absolute w-full h-full bg-black bg-opacity-10 group-hover:bg-opacity-30"></div>
                     <div className="absolute bottom-0 w-full h-[50%] bg-gradient-to-t from-[#a1b5ff5e]"></div>
-                    <img className={imageStyle} src="https://saintgermain.uy/cdn/shop/collections/Viennoiserie-categoria_900x.jpg"/>
+                    <img className={imageStyle} src={mainImageLink[0]}/>
                     <h3 className={textStyle}>VIENNOISERIES</h3>
                 </a>    
-                <a href="../collectionTienda" className={sonStyle}>
+                <a href={`../collectionTienda?mainImage=${mainImageLink[1]}&typeProduct=${textTypeProduct[1]}&products=${panes}`} className={sonStyle}>
                     <div className="absolute w-full h-full bg-black bg-opacity-10 group-hover:bg-opacity-30"></div>
                     <div className="absolute bottom-0 w-full h-[50%] bg-gradient-to-t from-[#a1b5ff5e]"></div>
-                    <img className={imageStyle} src="https://saintgermain.uy/cdn/shop/collections/panes-categoria_900x.jpg"></img>
+                    <img className={imageStyle} src={mainImageLink[1]}></img>
                     <h3 className={textStyle}>PANES</h3>
                 </a>
             </div>
             <div className={fatherStyle}>
-                <a href="../collectionTienda" className={sonStyle}>
+                <a href={`../collectionTienda?mainImage=${mainImageLink[2]}&typeProduct=${textTypeProduct[2]}&products=${patisserie}`} className={sonStyle}>
                     <div className="absolute w-full h-full bg-black bg-opacity-10 group-hover:bg-opacity-30"></div>
                     <div className="absolute bottom-0 w-full h-[50%] bg-gradient-to-t from-[#a1b5ff5e]"></div>
-                    <img className={imageStyle} src="https://saintgermain.uy/cdn/shop/collections/Painsuisse_1080x.jpg"></img>
+                    <img className={imageStyle} src={mainImageLink[2]}></img>
                     <h3 className={textStyle}>PATISSERIE</h3>
                 </a>
-                <a href="../collectionTienda" className={sonStyle}>
+                <a href={`../collectionTienda?mainImage=${mainImageLink[3]}&typeProduct=${textTypeProduct[3]}&products=${almuerzos}`} className={sonStyle}>
                     <div className="absolute w-full h-full bg-black bg-opacity-10 group-hover:bg-opacity-30"></div>
                     <div className="absolute bottom-0 w-full h-[50%] bg-gradient-to-t from-[#a1b5ff5e]"></div>
-                    <img className={imageStyle} src="https://saintgermain.uy/cdn/shop/collections/Quichequesodecabra_1080x.jpg"></img>
+                    <img className={imageStyle} src={mainImageLink[3]}></img>
                     <h3 className={textStyle}>ALMUERZOS</h3>
                 </a>
             </div>
